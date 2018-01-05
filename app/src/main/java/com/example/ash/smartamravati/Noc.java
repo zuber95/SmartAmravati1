ed776d3a9da3e49bfdcb866554ac973432693db4
Zuber <mohdzuber857@gmail.com>
2018-01-05 13:29:41 +0530

    Commit1

---
+++ app/src/main/java/com/example/ash/smartamravati/Noc.java
@@ -0,0 +1,13 @@
+package com.example.ash.smartamravati;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Noc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noc);
+    }
+}
