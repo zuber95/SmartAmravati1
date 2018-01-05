78718584a8217f0497e02bc2ad4614ff065ed29c
Zuber <mohdzuber857@gmail.com>
2018-01-05 17:03:02 +0530

    Commit2

---
+++ app/src/main/java/com/example/ash/smartamravati/activity/NocNext.java
@@ -0,0 +1,15 @@
+package com.example.ash.smartamravati.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ash.smartamravati.R;
+
+public class NocNext extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noc_next);
+    }
+}
