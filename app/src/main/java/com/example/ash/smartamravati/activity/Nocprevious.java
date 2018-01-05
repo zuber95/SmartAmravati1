c6d6fa55efb89e3488456552719e0ac33932f7c3
Zuber <mohdzuber857@gmail.com>
2018-01-05 18:50:14 +0530

    Commit2

--- app/src/main/java/com/example/ash/smartamravati/activity/Nocprevious.java
+++ app/src/main/java/com/example/ash/smartamravati/activity/Nocprevious.java
@@ -1,15 +1,31 @@
 package com.example.ash.smartamravati.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ash.smartamravati.R;
 
-public class Nocprevious extends AppCompatActivity {
+public class Nocprevious extends AppCompatActivity implements View.OnClickListener{
 
+    public Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nocprevious);
+        next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == next) {
+            Intent tp9 = new Intent(Nocprevious.this, NocNext.class);
+            startActivity(tp9);
+
+        }
+
     }
 }
