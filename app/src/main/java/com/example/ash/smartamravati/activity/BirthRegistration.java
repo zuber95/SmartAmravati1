478da2d9486eba307fd7203a661fcbd86a551b50
Zuber <mohdzuber857@gmail.com>
2018-01-07 00:59:34 +0530

    Commit2

--- app/src/main/java/com/example/ash/smartamravati/activity/BirthRegistration.java
+++ app/src/main/java/com/example/ash/smartamravati/activity/BirthRegistration.java
@@ -1,15 +1,29 @@
 package com.example.ash.smartamravati.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ash.smartamravati.R;
 
-public class BirthRegistration extends AppCompatActivity {
-
+public class BirthRegistration extends AppCompatActivity implements View.OnClickListener{
+public Button Next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_birth_registration);
+        Next=(Button)findViewById(R.id.Next);
+        Next.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==Next)
+        {
+            Intent tp1=new Intent(BirthRegistration.this,BirthRegistrationNext.class);
+            startActivity(tp1);
+        }
     }
 }
