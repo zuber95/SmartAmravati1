b74845394b4d2f6cc978334e97137e423be770a3
Zuber <mohdzuber857@gmail.com>
2018-01-07 00:30:43 +0530

    Commit2

--- app/src/main/java/com/example/ash/smartamravati/activity/BirthCertificate.java
+++ app/src/main/java/com/example/ash/smartamravati/activity/BirthCertificate.java
@@ -1,15 +1,29 @@
 package com.example.ash.smartamravati.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ash.smartamravati.R;
 
-public class BirthCertificate extends AppCompatActivity {
-
+public class BirthCertificate extends AppCompatActivity implements View.OnClickListener{
+public Button button9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_birth_certificate);
+        button9=(Button)findViewById(R.id.button9);
+        button9.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view==button9)
+        {
+            Intent tp9= new Intent(BirthCertificate.this,BirthRegistration.class);
+            startActivity(tp9);
+        }
     }
 }
