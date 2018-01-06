1d0a5aad7a2511d52a85b4f9a464b4f85cbd647c
Zuber <mohdzuber857@gmail.com>
2018-01-07 01:23:08 +0530

    Commit2

--- app/src/main/java/com/example/ash/smartamravati/activity/DeathCertificate.java
+++ app/src/main/java/com/example/ash/smartamravati/activity/DeathCertificate.java
@@ -1,15 +1,30 @@
 package com.example.ash.smartamravati.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ash.smartamravati.R;
 
-public class DeathCertificate extends AppCompatActivity {
-
+public class DeathCertificate extends AppCompatActivity implements View.OnClickListener {
+public Button button12;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_death_certificate);
+        button12=(Button)findViewById(R.id.button12);
+        button12.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view==button12)
+        {
+            Intent tp4 =new Intent(DeathCertificate.this,DeathRegistration.class);
+            startActivity(tp4);
+        }
     }
 }
--- app/src/main/java/com/example/ash/smartamravati/activity/DeathRegistration.java
+++ app/src/main/java/com/example/ash/smartamravati/activity/DeathRegistration.java
@@ -1,15 +1,30 @@
 package com.example.ash.smartamravati.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ash.smartamravati.R;
 
-public class DeathRegistration extends AppCompatActivity {
-
+public class DeathRegistration extends AppCompatActivity implements View.OnClickListener {
+public Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_death_registration);
+        btn=(Button)findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==btn)
+        {
+            Intent tp1=new Intent(DeathRegistration.this,DeathRegistrationNext.class);
+            startActivity(tp1);
+        }
+
     }
 }
